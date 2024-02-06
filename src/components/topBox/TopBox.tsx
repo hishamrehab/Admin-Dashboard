c2df5188d8d0d09ba7a15744642e3e18740777a5
hishamrehab <hishamrehab309@gmail.com>
2024-02-06 06:48:09 +0200

    last customers part

--- src/components/topBox/TopBox.tsx
+++ src/components/topBox/TopBox.tsx
@@ -1,16 +1,22 @@
-import React from "react";
 import { topDealUsers } from "../../data";
+import "../../components/topBox/TopBox.scss";
 const TopBox = () => {
   return (
     <div className="topBox">
-      <h1>Top Deals</h1>
+      <h1>Last customers</h1>
       <div className="list">
-        {topDealUsers.map((user) => {
+        {topDealUsers.map((user) => (
           <div className="listItem" key={user.id}>
-            <div className="user"></div>
-            <span className="amount">${useT}</span>
-          </div>;
-        })}
+            <div className="user">
+              <img src={user.img} alt="" />
+              <div className="userTexts">
+                <span className="username">{user.username}</span>
+                <span className="email">{user.email}</span>
+              </div>
+            </div>
+            <span className="amount">${user.amount}</span>
+          </div>
+        ))}
       </div>
     </div>
   );
