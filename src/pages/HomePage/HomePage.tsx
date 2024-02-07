5f8a1ba2fc38ee807f96691912c1a5aef3b7d321
hishamrehab <hishamrehab309@gmail.com>
2024-02-07 05:24:57 +0200

    chart part

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,3 +1,4 @@
+import ChartBox from "../../components/chartBox/ChartBox";
 import TopBox from "../../components/topBox/TopBox";
 import "./HomePage.scss";
 const HomePage = () => {
@@ -5,11 +6,19 @@ const HomePage = () => {
     <div className="home">
       <div className="box box1">
         <TopBox />
-         </div>
-      <div className="box box2">Box2 </div>
-      <div className="box box3">Box3 </div>
-      <div className="box box4">Box4 </div>
-      <div className="box box5">Box5 </div>
+      </div>
+      <div className="box box2">
+        <ChartBox />
+      </div>
+      <div className="box box3">
+        <ChartBox />
+      </div>
+      <div className="box box4">
+        <ChartBox />
+      </div>
+      <div className="box box5">
+        <ChartBox />
+      </div>
       <div className="box box6">Box6 </div>
       <div className="box box7">Box7 </div>
       <div className="box box8">Box8 </div>
